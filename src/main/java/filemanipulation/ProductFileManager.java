bdd58ab84228930f4f6df82241bf599166d479d2
SzabolcsMarton <maszabolcs@gmail.com>
2022-01-28 09:58:08 +0100

    filemanipulation task

---
+++ src/main/java/filemanipulation/ProductFileManager.java
@@ -0,0 +1,48 @@
+package filemanipulation;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductFileManager {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void readProductsFromFile(Path path) {
+        try {
+            addToProductsList(Files.readAllLines(path));
+        } catch (IOException exception) {
+            throw new IllegalStateException("Can't read file!", exception);
+        }
+    }
+
+    public void writePriceOverToFile(Path path, int minPrice) {
+        try {
+            Files.write(path, getLinesOverPrice(minPrice));
+        } catch (IOException exception) {
+            throw new IllegalStateException("Can not write file");
+        }
+    }
+
+    private List<String> getLinesOverPrice(int price) {
+        return products
+                .stream()
+                .filter(product -> Integer.parseInt(product.getPrice()) > price)
+                .map(Product::toString)
+                .toList();
+    }
+
+    private void addToProductsList(List<String> lines) {
+        lines.forEach(s -> {
+            String[] parts = s.split(";");
+            products.add(new Product(parts[0], parts[1], parts[2]));
+        });
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+}
